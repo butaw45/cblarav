7091898961903c82ee99471bdc733300460b8c7f
Muhammad Haris <kumantheas@gmail.com>
2021-08-21 10:24:59 +0700

    Ujicoba P2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Coba Blog | About</title>
-</head>
-<body>
+@extends('layouts.main')
     
+    @section('container')
     <h1>Halaman About</h1>
     <h3>{{ $nama }}</h3>
     <h3>{{ $no }}</h3>
     <h3>{{ $email }}</h3>
-    
-</body>
-</html>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-    // return view('welcome');
+    return view('home',[
+        "tittle" => "Home"
+    ]);
+    
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "tittle" => "About",
         "nama" => "Haris",
         "no" => "0304",
         "email" => "hariis12@pm.me"
@@ -27,6 +30,80 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+
+    
+$blog_post = [
+    [
+        "tittle" => "Judul Post Pertama",
+        "slug" => "Judul-post-Kedua",
+        "author" => "Author Name Pertama",
+        "body" => "
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti earum culpa aliquam molestiae
+            dolores maiores voluptatum minus fugiat saepe quibusdam? Eos voluptates aspernatur nisi recusandae
+            saepe voluptatum esse deleniti, numquam praesentium totam consequatur. Consequatur dolorum animi facere
+            quis rerum cum nihil ipsam asperiores, sint corporis qui ducimus, reprehenderit reiciendis eius?"
+    ],
+    [
+        "tittle" => "Judul Post Kedua",
+        "slug" => "Judul-post-Kedua",
+        "author" => "Author Name Kedua",
+        "body" => "
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam, praesentium ad fugit, alias tenetur quia 
+            dignissimos modi enim quibusdam commodi harum similique! Ea exercitationem, expedita porro illo quaerat reiciendis 
+            voluptates nisi voluptatibus ratione dolorum perferendis quis consequuntur modi laudantium dolores. Modi ratione, 
+            omnis quae saepe dolorum quam magnam veniam dicta rem sunt quisquam debitis nihil molestiae sequi repellat quis 
+            distinctio sapiente optio earum, amet dignissimos nobis consectetur nesciunt. Qui sapiente nulla illum voluptas 
+            excepturi tempora, rerum animi suscipit, fuga laboriosam, a itaque beatae rem aperiam! Aspernatur odio atque architecto 
+            consectetur ex dolore ratione, aut animi necessitatibus laborum incidunt ullam quo."
+    ]
+    ];
+
+
+    return view('posts',[
+        "tittle" => "Blog",
+        "posts" => $blog_post
+    ]);
 });
 
+//Halaman Sigle Post
+Route::get('posts/{slug}', function ($slug) {
+
+    $blog_post = [
+        [
+            "tittle" => "Judul Post Pertama",
+            "slug" => "Judul-post-Kedua",
+            "author" => "Author Name Pertama",
+            "body" => "
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti earum culpa aliquam molestiae
+                dolores maiores voluptatum minus fugiat saepe quibusdam? Eos voluptates aspernatur nisi recusandae
+                saepe voluptatum esse deleniti, numquam praesentium totam consequatur. Consequatur dolorum animi facere
+                quis rerum cum nihil ipsam asperiores, sint corporis qui ducimus, reprehenderit reiciendis eius?"
+        ],
+        [
+            "tittle" => "Judul Post Kedua",
+            "slug" => "Judul-post-Kedua",
+            "author" => "Author Name Kedua",
+            "body" => "
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam, praesentium ad fugit, alias tenetur quia 
+                dignissimos modi enim quibusdam commodi harum similique! Ea exercitationem, expedita porro illo quaerat reiciendis 
+                voluptates nisi voluptatibus ratione dolorum perferendis quis consequuntur modi laudantium dolores. Modi ratione, 
+                omnis quae saepe dolorum quam magnam veniam dicta rem sunt quisquam debitis nihil molestiae sequi repellat quis 
+                distinctio sapiente optio earum, amet dignissimos nobis consectetur nesciunt. Qui sapiente nulla illum voluptas 
+                excepturi tempora, rerum animi suscipit, fuga laboriosam, a itaque beatae rem aperiam! Aspernatur odio atque architecto 
+                consectetur ex dolore ratione, aut animi necessitatibus laborum incidunt ullam quo."
+        ]
+        ];
+
+        $new_post = [];
+        foreach($blog_post as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+        }
+
+    return view('post',[
+        "tittle" => "Sigle Post",
+        "post" => $new_post
+    ]);
+    
+});
