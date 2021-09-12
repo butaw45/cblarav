cdcee735534211a7f913f5bfc728935bba389e2c
Muhammad Haris <kumantheas@gmail.com>
2021-09-12 12:45:16 +0700

    Command & pengertian fungsi $fillable & $guarded

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
 {
     use HasFactory;
     //! Fungsi dari $fillable adalah yang bisa di isi  ['title','excerpt','body']; sisanya tidak bisa
-    protected $fillable = ['title','excerpt','body'];
+    // protected $fillable = ['title','excerpt','body'];
     //! Fungsi dari $guarded adalah kebalikan dari $fillable dimana ['id'] tidak bisa diisi sisanya bisa diisi
-    protected $guarded =  ['id']
+    protected $guarded =  ['id'];
 }
