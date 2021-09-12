b7465615f131b22ee7d951caf4ce8a5dd8f1fabc
Muhammad Haris <kumantheas@gmail.com>
2021-09-12 11:58:10 +0700

    Blade Escape Character

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,8 @@
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
     {{-- <h5>{{ $post["author"] }}</h5> --}}
-    <p>{{ $post->body }}</p>
+    {!! $post->body !!}
+    {{-- {{ $post->body }} --}}
 </article>
 <a href="/blog"> Back to Blog</a>
 @endsection
