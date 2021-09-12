6b687e3dbf8a6799b4ef60785627d1d53b5c2ea2
Muhammad Haris <kumantheas@gmail.com>
2021-09-12 12:37:38 +0700

    Command Pengertian dari masing-masing fungsi

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
+    //! Fungsi dari $fillable adalah yang bisa di isi  ['title','excerpt','body']; sisanya tidak bisa
     protected $fillable = ['title','excerpt','body'];
+    //! Fungsi dari $guarded adalah kebalikan dari $fillable dimana ['id'] tidak bisa diisi sisanya bisa diisi
+    protected $guarded =  ['id']
 }
